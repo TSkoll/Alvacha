e910b2052c1232d38369f0ac07b684db28a10f1a
lajavani <kopf.niskanen@gmail.com>
2020-03-06 20:20:39 +0200

    Adding TODO functions for average water consumption per time.

---
+++ datafunctions/index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const wData = require('./data/data.json')
+
+/*
+Function converts epoch time to a date object.
+@param gd = Given date as epochtime.
+@returns Date object.
+*/
+function convertToDate(gd) {
+  // TODO
+}
+
+/*
+Function calculates the average water consumption per year.
+*/
+function avgYearly() {
+ // TODO
+}
+
+/*
+Function calculates the average water consumption per month.
+*/
+function avgMonthly() {
+    // TODO
+}
+
+/*
+Function calculates the average water consumption per week.
+*/
+function avgWeekly() {
+    // TODO
+}
+
+/*
+Function calculates the average water consumption per day.
+*/
+function avgDaily() {
+    // TODO
+}
+
+
